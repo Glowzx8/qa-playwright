49d0de1bf499950b731055eef49e11bc2f41309c
Julien REDOIS <redois.julien@gmail.com>
2026-01-30 22:10:44 +0100

    test: add more edge case coverage

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,13 +25,14 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
       baseURL: 'https://katalon-demo-cura.herokuapp.com',
       trace: 'retain-on-failure',
-      screenshot: 'on-first-retry' : 'retain-on-failure',
+      screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */

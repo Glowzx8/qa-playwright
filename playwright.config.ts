bf814e35d195e74a45e575c3b6ee320137b12dbd
Julien REDOIS <redois.julien@gmail.com>
2026-01-30 22:04:41 +0100

    test: add more edge case coverage

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -31,7 +31,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
       baseURL: 'https://katalon-demo-cura.herokuapp.com',
       trace: 'retain-on-failure',
-      screenshot: 'only-on-failure',
+      screenshot: 'on-first-retry' : 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
